73e055afaaff200537215e297e8fa30d48b6ff36
HashirDevsinc <hashir.rehman@devsinc.com>
2018-07-09 13:44:53 +0500

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
 
   get '/help', to: 'static_pages#help'
